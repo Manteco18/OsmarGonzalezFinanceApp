43f7068998b6a09526212527a3d1ca76b23f704f
Osmar Gonzalez <osmarlolgg@gmail.com>
2026-03-14 11:59:24 -0600

    2

--- app/src/main/java/com/example/examenparcial_financeapp/MainActivity.kt
+++ app/src/main/java/com/example/examenparcial_financeapp/MainActivity.kt
@@ -1,38 +1,29 @@
 package com.example.examenparcial_financeapp
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LeadingIconTab
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.examenparcial_financeapp.ui.theme.ExamenParcial_FinanceAppTheme
-
 
 @Composable
 fun NewsScreen() {
@@ -42,36 +33,41 @@ fun NewsScreen() {
             .fillMaxSize()
             .background(Color.White)
             .padding(horizontal = 16.dp)
-            .verticalScroll(rememberScrollState()) // Permite scroll en toda la pantalla
+            .verticalScroll(rememberScrollState())
     ) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Row(
-
-            modifier = Modifier.fillMaxWidth(1f),
-            horizontalArrangement = Arrangement.SpaceBetween
-
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+
             Icon(
                 imageVector = Icons.Default.AccountCircle,
                 contentDescription = null,
-                modifier = Modifier.size(50.dp).clip(CircleShape).background(Color(0xFFFFE0B2)),
-
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color(0xFFFFE0B2))
+                    .padding(5.dp)
             )
 
             Column(
                 modifier = Modifier
-                    .weight(1f)   //  empuja el icono derecho
+                    .weight(1f)
                     .padding(horizontal = 10.dp)
             ) {
 
-                Text("Hola Osmar",
+                Text(
+                    text = "Hola Osmar",
                     fontSize = 25.sp
                 )
 
-                Text("Bienvenido"
-                    )
+                Text(
+                    text = "Bienvenido"
+                )
             }
 
             Icon(
@@ -79,6 +75,118 @@ fun NewsScreen() {
                 contentDescription = null,
                 modifier = Modifier.size(50.dp)
             )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        ) {
+
+            Card(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
+                shape = RoundedCornerShape(20.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFD7E3DD))
+            ) {
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp)
+                    )
+
+                    Text(
+                        text = "Actividad",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+
+                    Text(
+                        text = "de la Semana",
+                        color = Color.Gray
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                // Card ventas
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFEADDD1))
+                ) {
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+
+                        Text(
+                            text = "Ventas",
+                            color = Color.Gray
+                        )
+
+                        Text(
+                            text = "$280.99",
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                // Card ganancias
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    shape = RoundedCornerShape(20.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFD9D4EA))
+                ) {
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+
+                        Text(
+                            text = "Ganancias",
+                            color = Color.Gray
+                        )
+
+                        Text(
+                            text = "$280.99",
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
+        }
+        Row(
+
+        ) {
 
         }
     }
